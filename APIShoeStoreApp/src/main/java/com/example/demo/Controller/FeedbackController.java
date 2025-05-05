6a91fbd2519b59207aa4fa57184fedb413ac1ad7
Manh Pham <164108293+PhamNguyenTienManh@users.noreply.github.com>
2025-05-05 09:35:34 +0700

    test

--- APIShoeStoreApp/src/main/java/com/example/demo/Controller/FeedbackController.java
+++ APIShoeStoreApp/src/main/java/com/example/demo/Controller/FeedbackController.java
@@ -3,15 +3,15 @@ package com.example.demo.Controller;
 
 import com.example.demo.DTO.Request.FeedbackRequest;
 import com.example.demo.DTO.Response.APIResponse;
+import com.example.demo.DTO.Response.FeedbackResponse;
 import com.example.demo.Entity.Feedback;
 import com.example.demo.Service.FeedbackService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +24,13 @@ public class FeedbackController {
     public APIResponse<Boolean> createFeedback(@RequestBody FeedbackRequest feedbackRequest) {
         APIResponse<Boolean> response = new APIResponse<>();
         response.setResult(feedbackService.saveFeedback(feedbackRequest));
+        return response;
+    }
+
+    @GetMapping("/{name}")
+    public APIResponse<List<FeedbackResponse>> getFeedbacksByProduct(@PathVariable String name) {
+        APIResponse<List<FeedbackResponse>> response = new APIResponse<>();
+        response.setResult(feedbackService.getFeedbacksByProduct(name));
         return response;
     }
 }
--- APIShoeStoreApp/src/main/java/com/example/demo/Service/FeedbackService.java
+++ APIShoeStoreApp/src/main/java/com/example/demo/Service/FeedbackService.java
@@ -2,10 +2,14 @@ package com.example.demo.Service;
 
 
 import com.example.demo.DTO.Request.FeedbackRequest;
+import com.example.demo.DTO.Response.FeedbackResponse;
 import com.example.demo.Entity.Account;
 import com.example.demo.Entity.Feedback;
 import com.example.demo.Entity.Product;
+import com.example.demo.Exception.AppException;
+import com.example.demo.Exception.ErrorCode;
 import com.example.demo.Repository.FeedbackRepository;
+import com.example.demo.Repository.ProductRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -13,13 +17,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
 public class FeedbackService {
-    private FeedbackRepository feedbackRepository;
+    FeedbackRepository feedbackRepository;
+    ProductRepository productRepository;
 
     public boolean saveFeedback(FeedbackRequest feedbackRequest) {
         Feedback feedback = new Feedback();
@@ -35,5 +42,25 @@ public class FeedbackService {
         feedbackRepository.save(feedback);
         return true;
     }
+
+    public List<FeedbackResponse> getFeedbacksByProduct(String productName) {
+        Product product = productRepository.findByName(productName)
+                .orElseThrow(()->new AppException(ErrorCode.PRODUCT_NOT_FOUND));
+        List<Feedback> feedbackList = feedbackRepository.findByProductId(product.getId());
+
+        List<FeedbackResponse> responseList = new ArrayList<>();
+        for (Feedback feedback : feedbackList) {
+            FeedbackResponse response = new FeedbackResponse(
+                    feedback.getComment(),
+                    feedback.getRate(),
+                    feedback.getAccount().getFullname()
+            );
+            responseList.add(response);
+        }
+
+        return responseList;
+    }
+
+
 
 }
